24f52854668920ddbf9074b4962dbb53bbdf7293
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-04 16:54:02 -0300

    Criando o tema.

---
+++ src/styles/Theme/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react'
+export const theme = extendTheme({
+  colors: {},
+  fonts: {
+    heading: 'Playfair Display sans-serif',
+    text: 'Raleway sans-serif',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: '#FFFFFF',
+      },
+    },
+  },
+})
