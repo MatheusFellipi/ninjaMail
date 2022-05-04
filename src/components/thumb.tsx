b58700ad1a8e9c17e1b0d7d3c6703dd1b48d8e30
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-04 19:51:56 -0300

    Terminando os estilos do componente.

--- src/components/thumb.tsx
+++ src/components/thumb.tsx
@@ -1,17 +1,35 @@
 import { Box, Image } from '@chakra-ui/react'
+import Link from 'next/link'
 
 const Thumb = () => {
   return (
-    <Box
-      maxW={209}
-      h={275}
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-    >
+    <Box maxW={209} h={294} borderWidth="1px" shadow={'lg'}>
       <Image src="/img/thumb.png" alt="thumb.png" />
 
-      <Box p="6"></Box>
+      <Box px={'25px'}>
+        <Box
+          as="p"
+          fontSize={'12px'}
+          lineHeight={'16px'}
+          fontFamily={'text'}
+          fontWeight={500}
+        >
+          Launch campaigns but also find new customers. Our unique platform
+          handles campaigns from start to end.
+        </Box>
+        <Link href="#">
+          <Box
+            as={'a'}
+            fontSize={'12px'}
+            lineHeight={'14px'}
+            fontFamily={'text'}
+            fontWeight={800}
+            color="#4BA87D"
+          >
+            Learn More &gt;
+          </Box>
+        </Link>
+      </Box>
     </Box>
   )
 }
