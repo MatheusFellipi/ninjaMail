a1be16ae66d6d7b1e2856eeb47a11ab8c9ce6bfd
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-04 19:31:53 -0300

    Criando o componente cabeçalhos.

---
+++ src/components/heeader.tsx
@@ -0,0 +1,46 @@
+import { Box, Button, Flex, Heading, Image } from '@chakra-ui/react'
+
+const Header = () => {
+  return (
+    <Flex
+      as={'header'}
+      w={'100%'}
+      h={'62px'}
+      justify={'space-around'}
+      align={'center'}
+    >
+      <Flex w={'155px'} h={'52px'} align={'center'}>
+        <Box width={84} height={42} mt={'12px'}>
+          <Image src="/icon/ninjamail.svg" alt="ninjamail" />
+        </Box>
+        <Heading
+          fontSize="22px"
+          lineHeight={'26px'}
+          fontStyle={'normal'}
+          fontFamily={'text'}
+          h={'25px'}
+          fontWeight={'black'}
+          ml={'-10px'}
+        >
+          NinjaMail
+        </Heading>
+      </Flex>
+      <Box></Box>
+      <Box>
+        <Button
+          lineHeight={'13px'}
+          fontSize="11px"
+          fontFamily={'text'}
+          colorScheme={'green'}
+          borderRadius={0}
+          color={'white'}
+          fontWeight={'extrabold'}
+        >
+          SIGN UP FREE
+        </Button>
+      </Box>
+    </Flex>
+  )
+}
+
+export default Header
