38a87cb543e95a1bdf4b40a88fd2f21af12b0d00
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-05 11:41:38 -0300

    Fazendo o responsivo no componente.

--- src/components/thumb.tsx
+++ src/components/thumb.tsx
@@ -3,7 +3,12 @@ import Link from 'next/link'
 
 const Thumb = () => {
   return (
-    <Box maxW={209} h={294} borderWidth="1px" shadow={'lg'}>
+    <Box
+      w={{ xl: 320, md: 209.28, sm: 209 }}
+      h={{ xl: 198, md: 290.16, sm: 294 }}
+      borderWidth="1px"
+      shadow={'lg'}
+    >
       <Image src="/img/thumb.png" alt="thumb.png" />
 
       <Box px={'25px'} mt={'17px'}>
